022b825d0d6854a14361899927540a063a575f47
pavan5745 <112757658+pavan5745@users.noreply.github.com>
2024-08-25 09:17:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://my-hostel.onrender.com/"], // React frontend URL
+    origin: ["http://localhost:3000", "https://my-hostel.onrender.com"], // Corrected React frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
