7c763de5fb52c37189613317b2af69d65309c34d
pavan5745 <112757658+pavan5745@users.noreply.github.com>
2024-08-26 13:25:25 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -111,7 +111,7 @@ exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles ['admin', 'lead-guide']. role='user'
 
-    if (roles.indexOf("admin") < 0) {
+    if (roles.indexOf(req.user.role) < 0) {
       return next(
         new AppError("You do not have permission to perform this action", 403)
       );
